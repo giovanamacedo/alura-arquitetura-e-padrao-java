87e9e9fadb77c12745614d94f3df867d7777a62c
giovanamacedo <giovanaml12@gmail.com>
2023-05-21 23:45:36 -0300

    adicionando arquivos do curso de tdd

---
+++ src/ModuloTres/service/BonusService.java
@@ -0,0 +1,17 @@
+package ModuloTres.service;
+
+import java.math.BigDecimal;
+
+import ModuloTres.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
