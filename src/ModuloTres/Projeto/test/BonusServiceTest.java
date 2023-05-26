41bb4764d6aa1d044c8511be7c6cdd2b42e10fe9
giovanamacedo <giovanaml12@gmail.com>
2023-05-25 21:32:20 -0300

    implementando testes

--- src/ModuloTres/Projeto/test/BonusServiceTest.java
+++ src/ModuloTres/Projeto/test/BonusServiceTest.java
@@ -11,17 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusServiceTest {
 
-    @Test public void bonusDeveriaSerZeroParaSalarioAlto(){
+    @Test
+    public void bonusDeveriaSerZeroParaSalarioAlto(){
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Giovana", LocalDate.now(), new BigDecimal("25000")));
 
         assertEquals(BigDecimal.ZERO, bonus);
     }
 
-    @Test public void bonusDeveriaSerDezPorcentoDoSalario(){
+    @Test
+    public void bonusDeveriaSerDezPorcentoDoSalario(){
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Giovana", LocalDate.now(), new BigDecimal("2500")));
 
-        assertEquals(new BigDecimal("250"), bonus);
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSerDezPorcentoParaDezMil(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Giovana", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.0"), bonus);
     }
 }
